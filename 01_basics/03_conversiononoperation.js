a752b585264738ce0da21af493786141d6873722
Suraj Kumar <155898398+Suraj5119@users.noreply.github.com>
2024-01-17 19:10:50 +0000

    conversion of operators

--- 01_basics/03_conversiononoperation.js
+++ 01_basics/03_conversiononoperation.js
@@ -46,14 +46,15 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);  //12
+// console.log(1 + "2");   //12
+// console.log("1" + 2 + 2);  //122
+// console.log(1 + 2 + "2");  //32 isme aage ka add kr denge
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
+//// console.log(true);  //true
+// console.log(+true);  // 1 
 // console.log(+"");
 
 let num1, num2, num3
