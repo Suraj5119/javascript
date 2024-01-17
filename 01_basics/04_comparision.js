c3ff373efaea5b052ced045e90739a093a27fb54
Suraj Kumar <155898398+Suraj5119@users.noreply.github.com>
2024-01-17 20:46:52 +0000

    comparision operator

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);  //true
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);  //true bcoz 2 ko number me convert kr dega
+// console.log("02" > 1);  //true
+
+
+//avoid below conversion
+console.log(null > 0);  //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+
+// === 
+
+console.log("2" === 2);  // value and datatypes dono check krega
