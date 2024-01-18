78e49a79eedf5c0bd0f0dee95146fa9527170184
Suraj Kumar <155898398+Suraj5119@users.noreply.github.com>
2024-01-18 06:39:35 +0000

    array part 02

---
+++ 02_array/02.js
@@ -0,0 +1,30 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros), //last element will be complete dc_heroes ie ans = ["thor", "Ironman", "spiderman",["superman", "flash", "batman"]]
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]); //thor
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros); // ["thor", "Ironman", "spiderman","superman", "flash", "batman"]
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros); //["thor", "Ironman", "spiderman","superman", "flash", "batman"]
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array); //
+
+
+
+console.log(Array.isArray("Hitesh")) // false //is Hitesh array?? => no=>false
+console.log(Array.from("Hitesh")) //[H,i,t,e,s,h]
+console.log(Array.from({name: "hitesh"})) // interesting case ans = [] empty array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // returns new array as set of all element [100,200,300]
