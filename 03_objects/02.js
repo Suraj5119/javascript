4929f44407b8943f39ed0d621fa4b74311cf7cfe
Suraj Kumar <155898398+Suraj5119@users.noreply.github.com>
2024-01-21 13:17:59 +0000

    object part 2

---
+++ 03_objects/02.js
@@ -0,0 +1,84 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname); //hitesh
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4) //{1: "a", 2: "b", 3: "a", 4: "b"};
+
+const obj3 = {...obj1, ...obj2} 
+// console.log(obj3);  // {1: "a", 2: "b", 3: "a", 4: "b"};
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));  //array ke andar sarri keys aa jayegi
+// console.log(Object.values(tinderUser));//array ke andar sarri values aa jayegi
+// console.log(Object.entries(tinderUser)); 
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));// true // is name ki koi property hai ki nhi
+
+ 
+// ******************************************
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);  // hitesh
+console.log(instructor);  //hitesh
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
