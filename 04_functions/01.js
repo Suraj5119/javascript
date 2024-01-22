ba9632fd80bc751639afd38d21a66188d8ebe83c
Suraj Kumar <155898398+Suraj5119@users.noreply.github.com>
2024-01-22 07:32:49 +0000

    function basic

--- 04_functions/01.js
+++ 04_functions/01.js
@@ -15,58 +15,47 @@ function sayMyName(){
 //     console.log(number1 + number2);
 // }
 
-function addTwoNumbers(number1, number2){ //parameter
+function addTwoNumbers(number1, number2){
 
     // let result = number1 + number2
     // return result
     return number1 + number2
 }
 
-const result = addTwoNumbers(3, 5) //arguments
+const result = addTwoNumbers(3, 5)
 
 // console.log("Result: ", result);
 
 
-function loginUserMessage(username = "sam"){ // "sam" is default username
-    if(!username){ //
+function loginUserMessage(username = "sam"){
+    if(!username){
         console.log("PLease enter a username");
-        return ;
+        return
     }
-    return `${username} just logged in` ;
+    return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage())  // if we did not pass anything then ans is undefined
+// console.log(loginUserMessage("hitesh"))
 
 
-
-
-
-
-
-
-
-function calculateCartPrice(val1, val2, ...num1){ 
-    return num1; //num1 will return [500,2000] 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
 }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 
-
-
-
 const user = {
     username: "hitesh",
-    price: 199
+    prices: 199
 }
 
 function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
+
 // handleObject(user)
-
-
-handleObject({ // we can directly pass object.
+handleObject({
     username: "sam",
     price: 399
 })
@@ -78,4 +67,4 @@ function returnSecondValue(getArray){
 }
 
 // console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000])); //// we can directly pass array.
+console.log(returnSecondValue([200, 400, 500, 1000]));
