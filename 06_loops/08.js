976f42868e2ef4b697f20950853d2b646742471b
Suraj Kumar <155898398+Suraj5119@users.noreply.github.com>
2024-01-22 07:58:53 +0000

    loops

--- 06_loops/08.js
+++ 06_loops/08.js
@@ -3,7 +3,7 @@ const myNums = [1, 2, 3]
 // const myTotal = myNums.reduce(function (acc, currval) {
 //     console.log(`acc: ${acc} and currval: ${currval}`);
 //     return acc + currval
-// }, 0)
+// }, 0)//starting value of acc
 
 const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
